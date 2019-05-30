355356391539df4111a437431866af7b0bd8a46b
RosemaryGonzaga <rosemary.gonzaga@gmail.com>
2019-05-30 19:36:40 -0400

    User Auth: one tiny change - added under construction message to splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -7,7 +7,7 @@ const Splash = props => {
 
     return (
         <div>
-            <h2>This is the splash page. Please sign in!</h2>
+            <h2>This is the splash page. In progress, but please visit this route and throw some errors at it: /#/login_page</h2>
             <Link to="/login">Log In</Link>
             <Link to="/signup">Try for Free</Link>
         </div>

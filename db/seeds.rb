e9d65d978d625f24fcad0bc1bc390f75565c41b3
RosemaryGonzaga <rosemary.gonzaga@gmail.com>
2019-05-30 20:43:28 -0400

    Seed db with demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create!({
+    primary_email: "user1@shavasana.com",
+    password: "password",
+})

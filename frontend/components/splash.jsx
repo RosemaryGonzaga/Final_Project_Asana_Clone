ebdee8f4d1000aff85103753191a9ba5e7db60cd
RosemaryGonzaga <rosemary.gonzaga@gmail.com>
2019-05-30 22:27:49 -0400

    User Auth: Started styling splash page, still in progress

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -6,10 +6,30 @@ import { connect } from 'react-redux';
 const Splash = props => {
 
     return (
-        <div>
-            <h2>This is the splash page. In progress, but please visit this route and throw some errors at it: /#/login_page</h2>
-            <Link to="/login">Log In</Link>
-            <Link to="/signup">Try for Free</Link>
+        <div className="splash-container">
+            <header className="splash-main-nav">
+                <div className="splash-left-nav">
+                    <img src={window.asanaLogoSplash} alt="logo"/>
+                </div>
+                <nav className="splash-right-nav">
+                    <ul>
+                        <a href="">Templates</a>
+                        <a href="">Product</a>
+                        <a href="">Pricing</a>
+                        <a href="">Solutions</a>
+                        <a href="">Contact Sales</a>
+                        <Link to="/login">Log In</Link>
+                        <div className="free-trial-button"><Link to="/signup">Try for Free</Link></div>
+                    </ul>
+                </nav>
+            </header>
+
+            <div className="splash-background">
+                <h2>This is the splash page.</h2>
+                <p>It's still under construction.</p>
+                <p>In the meantime, please visit this route and throw some errors at it: /#/login_page</p>
+                <p>To do: add modal, images, signup form...</p>
+            </div>
         </div>
     );
 };

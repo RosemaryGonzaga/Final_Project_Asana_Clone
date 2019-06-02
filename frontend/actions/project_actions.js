c25b776d98259b6fc39c4997fcfaa648288ce65a
RosemaryGonzaga <rosemary.gonzaga@gmail.com>
2019-06-02 18:36:17 -0400

    Projects: Write regular and thunk action creators, plus action types

---
+++ frontend/actions/project_actions.js
@@ -0,0 +1,67 @@
+import * as ProjectApiUtil from '../util/projects_api_util';
+
+// action types
+export const RECEIVE_ALL_PROJECTS = "RECEIVE_ALL_PROJECTS";
+export const RECEIVE_PROJECT = "RECEIVE_PROJECT";
+export const REMOVE_PROJECT = "REMOVE_PROJECT";
+
+
+// regular action creators
+const receiveAllProjects = projects => {
+    return {
+        type: RECEIVE_ALL_PROJECTS,
+        projects,
+    };
+};
+
+const receiveProject = project => {
+    return {
+        type: RECEIVE_PROJECT,
+        project,
+    };
+};
+
+const removeProject = projectId => {    // double check later
+    return {
+        type: REMOVE_PROJECT,
+        projectId,
+    };
+};
+
+
+// thunk action creators
+
+export const fetchProjects = () => {
+    return dispatch => {
+        return ProjectApiUtil.fetchProjects()
+            .then(payload => dispatch(receiveAllProjects(payload)));
+    };
+}
+
+export const fetchProject = id => {
+    return dispatch => {
+        return ProjectApiUtil.fetchProject(id)
+            .then(payload => dispatch(receiveProject(payload)));
+    };
+}
+
+export const createProject = project => {
+    return dispatch => {
+        return ProjectApiUtil.createProject(project)
+            .then(payload => dispatch(receiveProject(payload)));
+    };
+}
+
+export const updateProject = project => {
+    return dispatch => {
+        return ProjectApiUtil.updateProject(project)
+            .then(payload => dispatch(receiveProject(payload)));
+    };
+}
+
+export const deleteProject = id => {
+    return dispatch => {
+        return ProjectApiUtil.deleteProject(id)
+            .then(payload => dispatch(removeProject(id)));  // double check later
+    };
+}

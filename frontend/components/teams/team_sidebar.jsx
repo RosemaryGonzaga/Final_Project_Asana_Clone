c95ca40fa278e7642a054c43130c51c42f0c27f3
RosemaryGonzaga <rosemary.gonzaga@gmail.com>
2019-07-08 15:32:13 -0400

    Style team index in sidebar component (team member intitials)

--- frontend/components/teams/team_sidebar.jsx
+++ frontend/components/teams/team_sidebar.jsx
@@ -11,19 +11,31 @@ class TeamSidebar extends React.Component {
         // Opt 1: filter here in this component?
         // Opt 2: modify fetchUsers / Users#index (controller) to only fetch current team's users instead of all users in DB?
         // Opt 3: write a parallel fetchUsers / Users#index to allow for both scenarios (will need to fetch all users when inviting new team members?)
-        const teamMembers = users.map(user => {
+        // Implemented option 2
+        let teamMembers = users.map(user => {
             const initials = user.primaryEmail.slice(0, 2).toUpperCase();
-            return <li>{initials}</li>;
+            return <li className="team-sidebar-initials avatar" key={user.id}>{initials}</li>;
         });
+
+        if (teamMembers.length <= 3) {
+            let i = 1;
+            while (teamMembers.length < 3) {
+                teamMembers.push(<li className="team-sidebar-initials-placeholder" key={`blank${i}`}></li>)
+                i++;
+            }
+            teamMembers.push(<li className="team-sidebar-initials-invite-ppl" key="invite-ppl">Invite People</li>)
+        }
+
+        // teamMembers = teamMembers.slice(0, 7);    // only show first 7 team members 
 
         let teamProjects = projects.slice();
         let projectItems = null;
         if (currentTeam) {  // only access currentTeam's id if currentTeam is truthy
             teamProjects = projects.filter(project => project.teamId === currentTeam.id);
-            projectItems = teamProjects.map(project => <li>{project.name}</li>);
+            projectItems = teamProjects.map(project => <li key={project.id}>{project.name}</li>);
         }
 
-        // const teamProjects = projects.map(project => <li>{project.name}</li>);
+        // projectItems = projectItems.slice(0, 5);    // only show first 5 projects
 
         // TEMPORARY?
         const userTeams = teams.map(team => <li>{team.name}</li>);
@@ -32,11 +44,11 @@ class TeamSidebar extends React.Component {
             <div className="team-sidebar-container">
                 <div className="team-sidebar-heading">
                     <h2>{currentTeam ? currentTeam.name : null}</h2>
-                    <div>+</div>
+                    <i className="fas fa-plus"></i>
                 </div>
                 <br /> {/* TEMP BREAK */}
                 <ul className="team-sidebar-members">
-                    <li>{currentTeam ? currentTeam.name : null} members:</li>
+                    {/* <li>{currentTeam ? currentTeam.name : null} members:</li> */}
                     {teamMembers}
                 </ul>
                 <br /> {/* TEMP BREAK */}
